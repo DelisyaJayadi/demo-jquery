70966c430108e3146b4dcf147c291af429cfb781
unknown <delisyajayadi10@gmail.com>
2023-05-14 22:43:19 +0700

    progress satu

---
+++ controller.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $text = $_POST['teks'];
+    $number = $_POST['angka'];
+
+    $my_array = array();
+    for ($i = 0; $i < $number; $i++) {
+        $my_array[$i] = $i."-".$text;
+    };
+
+}
+
+?>
